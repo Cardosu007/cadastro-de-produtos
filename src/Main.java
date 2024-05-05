810a64b36aa69355ea4dbcb1360309681fb44120
W Cardoso <cardosu7@gmail.com>
2024-05-05 09:02:27 -0300

    Finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+
+        cadastroProdutos.adicionarProduto("Produto 5", 1L, 15d, 5);
+        cadastroProdutos.adicionarProduto("Produto 2", 2L, 25d, 2);
+        cadastroProdutos.adicionarProduto("Produto 3", 3L, 5d, 3);
+        cadastroProdutos.adicionarProduto("Produto 4", 4L, 18d, 9);
+        cadastroProdutos.adicionarProduto("Produto 1", 5L, 37d, 1);
+
+        //System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+
+
+
     }
 }
